ae8216851065fc56039a5c6800b4aa26be7b03b1
proezels <miepsons@gmail.com>
2020-06-21 23:43:49 +0200

    Final Version :]

---
+++ BPW 2/Assets/music.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class music : MonoBehaviour
+{
+    private static music _instance;
+    private void Awake()
+    {
+        if (!_instance)
+        {
+            _instance = this;
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+        
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    public void audioContinue()
+    {
+        DontDestroyOnLoad(transform.gameObject);
+    }
+}
